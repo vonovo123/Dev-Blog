7c2c929c68a0cd342ab6d9f31d0147be09706ae8
vonovo123 <vonovo123@naver.com>
2026-07-19 23:35:00 +0900

    rewrite head-meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,11 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <meta property="og:title" content={"Dynamic_Kwon Dev Blog"} />
+        <meta property="og:title" content={"금융IT 개발자 권현우 개발 블로그"} />
         <meta property="og:type" content="website" />
         <meta
           name="description"
-          content={"금융권 프론트엔드 개발자 권현우의 기술 블로그입니다."}
+          content={"금융IT 개발자 권현우의 기술 블로그입니다."}
         />
         <meta property="og:article:author" content="dynamic_kwon" />
       </Head>
