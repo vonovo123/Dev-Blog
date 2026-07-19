08da723141408feffd6356da6b1924ed693ffa7b
vonovo123 <vonovo123@naver.com>
2026-07-19 23:38:38 +0900

    rewrite head-meta

--- components/HeadMeta.jsx
+++ components/HeadMeta.jsx
@@ -4,7 +4,7 @@ const HeadMeta = ({ url, image }) => {
   return (
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:url" content={url || "https://dynamic-kwon.dev"} />
+      <meta property="og:url" content={url || "https://vonovo123.vercel.app"} />
       <meta property="og:image" content={image} />
     </Head>
   );
